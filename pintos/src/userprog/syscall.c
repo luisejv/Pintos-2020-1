4d4c2b13e37e3bd5799cc3033a0b41c060a16d68
Marcuss17 <eduardo.medina@utec.edu.pe>
2020-04-23 01:13:08 +0000

    Starting to build the sys handler

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -12,9 +12,38 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+int
+write (int fd, void* buffer, unsigned size){
+  printf("Inside Write\n");
+  return 1;
+}
+
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
+syscall_handler (struct intr_frame *f) 
+{ 
+  int syscallName = *(int*)f->esp;
+
+  switch(syscallName){
+    case SYS_EXIT:{
+      printf("Exiting the process\n");
+      
+      break;
+    }
+    case SYS_READ:{
+      break;
+    }
+    case SYS_WRITE:{
+      //El puntero sube porque se recibe la data de arriba para abajo, estando el number
+      //Del syscall debajo.
+      int fd = *((int*)f->esp+1); 
+      void* buffer = (void*)(*((int*)f->esp+2));
+      unsigned size = *((unsigned*)f->esp+3);
+      
+      f->eax = write(fd,buffer,size);
+    }
+    
+    //Faltan implementar los others sys_calls
+  }
   printf ("system call!\n");
   thread_exit ();
 }
--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -1,6 +1,11 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "process.h"
+
 void syscall_init (void);
+int write(int, void*, unsigned);
+void exit(int status);
+pid_t exec(const char* cmd_line); 
 
 #endif /* userprog/syscall.h */
